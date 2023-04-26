833ffdd3f4cd91c99b4a34249bc0348a84f55f55
Anurag7275977 <arjunsharma705463@gmail.com>
2023-04-27 02:22:53 +0530

    Product card listing Assignmnet

--- App.js
+++ App.js
@@ -1,39 +1,44 @@
-import './App.css';
-import React from 'react';
-import Profilecard from './Component/Profilecard'
+// App.js
 
-const users = [
-  {
-    image: 'https://imageio.forbes.com/specials-images/imageserve/6176e6bb6b6152e811db62ea/0x0.jpg?format=jpg&height400=&width=300',
-    designation: 'CEO Microsoft',
-    name: 'Bill Gates',
-    jobDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiPiPbHLkig6p3Xb2FqqRURvT_4vA2UCjHqg&usqp=CAU',
-    designation: 'CEO Twitter',
-    name: 'Elon musk',
-    jobDescription: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem.',
-  },
-  {
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYELh6xwuDriJ8ioyBB_ClmpM42I-jPlytKMFXbYZ2-J5iCUrm6R8S-8xFU073FmwPTHo&usqp=CAU',
-    designation: 'Founder of Amazon ',
-    name: 'Jeff Bezos',
-    jobDescription: 'At vero eos et accusamus et iusto odio dignissimos ducimus.',
-  },
-];
+import React from 'react';
+import ProductCard from './ProductCard';
+import './App.css';
 
 function App() {
+  const productDetails = [
+    {
+      productId: '1',
+      productPic: 'https://i.pinimg.com/736x/2b/e9/4d/2be94de3d0f1ee523da6fa68118ebf96.jpg',
+      productName: 'Sports Shoes',
+      brand: 'Adidas',
+      productDescription: 'Lightweight and comfortable sports shoes for all types of activities.',
+      price: '4000 INR',
+      rating: 4,
+    },
+    {
+      productId: '2',
+      productPic: 'https://m.media-amazon.com/images/I/51lIbavjJFL._SY450_.jpg',
+      productName: 'Men\'s Watch',
+      brand: 'Titan',
+      productDescription: 'Stylish and durable men\'s watch with leather strap.',
+      price: '2500 INR',
+      rating: 3.5,
+    },
+    {
+      productId: '3',
+      productPic: 'https://m.media-amazon.com/images/I/61v7JxwCj+L._SY450_.jpg',
+      productName: 'Laptop',
+      brand: 'HP',
+      productDescription: 'Powerful and versatile laptop for all your computing needs.',
+      price: '60000 INR',
+      rating: 4.5,
+    },
+  ];
+
   return (
-    <div className="App">
-       {users.map((user) => (
-        <Profilecard
-          // key={user.name}
-          Name={user.name}
-          image={user.image}
-          designation={user.designation}
-          jobDescription={user.jobDescription}
-        />
+    <div className="product-cards-container">
+      {productDetails.map(productDetail => (
+        <ProductCard key={productDetail.productId} productDetail={productDetail} />
       ))}
     </div>
   );
